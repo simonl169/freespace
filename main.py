052ea5d378406a8d173eef273fc05a2aca18ceb6
simonl169 <simon@ragnar24.com>
2023-02-10 20:14:43 +0100

    added new function in utils.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from utils import show_disk_space
+from utils import show_disk_space, show_free_perc
 
 if __name__ == "__main__":
     print("Hello, World!")
+    print("")
 
     show_disk_space()
+
+    show_free_perc()
--- utils.py
+++ utils.py
@@ -5,8 +5,22 @@ def show_disk_space():
 
     total, used, free = shutil.disk_usage("/")
 
-    print("Total: %d GiB" % (total // (2**30)))
-    print("Used: %d GiB" % (used // (2**30)))
-    print("Free: %d GiB" % (free // (2**30)))
+    if __name__ == "__main__":
+        print("Total: %d GiB" % (total // (2**30)))
+        print("Used: %d GiB" % (used // (2**30)))
+        print("Free: %d GiB" % (free // (2**30)))
 
     return total, used, free
+
+def show_free_perc():
+    total, used, free = show_disk_space()
+    percentage = free/total*100
+
+    print("Total: %d GiB" % (total // (2 ** 30)))
+    print("Used: %d GiB" % (used // (2 ** 30)))
+    print("Free: %d GiB" % (free // (2 ** 30)))
+    print("")
+
+    print("Free: %d %%" % (percentage))
+
+    return percentage
